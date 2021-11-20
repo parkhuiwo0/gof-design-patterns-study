6ebcb755543603484f815df7f7754d7ead1cc627
kurly-huiwoo <huiwoo.park@kurlycorp.com>
2021-11-21 02:51:30 +0900

    initalize commit

---
+++ src/test/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/GofDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
