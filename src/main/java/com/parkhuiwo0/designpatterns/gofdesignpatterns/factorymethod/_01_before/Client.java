0d37e4c0d5b32905ae0724f4b44878ccd23e5452
parkhuiwo0 <huiwo0@naver.com>
2021-11-28 23:36:11 +0900

    [Factory Method] - 리팩토링 전 코드 추가

---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/factorymethod/_01_before/Client.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns.factorymethod._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Ship whiteship = ShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        System.out.println(whiteship);
+
+        Ship blackship = ShipFactory.orderShip("Blackship", "keesun@mail.com");
+        System.out.println(blackship);
+    }
+
+}
