07f17e895c90af7806fd08c8455630d2bafb4d49
parkhuiwo0 <huiwo0@naver.com>
2021-11-29 00:06:16 +0900

    [Factory Method] - 사용하는 클라이언트가 Interface에 의존하게 만들어서 결합을 제거한다. (DI의 활용)

--- src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/factorymethod/after/Client.java
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/factorymethod/after/Client.java
@@ -4,13 +4,21 @@ public class Client {
 
     public static void main(String[] args) {
 
-        Ship ship = new WhiteShipFactory().orderShip("Whiteship", "parkhuiwo0@gmail.com");
+        Client client = new Client();
 
+        Ship ship = new WhiteShipFactory().orderShip("Whiteship", "parkhuiwo0@gmail.com");
         System.out.println(ship);
 
         Ship blackship = new BlackShipFactory().orderShip("Blackship", "parkhuiwo0@gmail.com");
-
         System.out.println(blackship);
+
+        client.print(new WhiteShipFactory(), "whiteship", "parkhuiwo0@gmail.com");
+        client.print(new WhiteShipFactory(), "blackship", "parkhuiwo0@gmail.com");
+
+    }
+
+    private void print(ShipFactory shipFactory, String name, String email) {
+        System.out.println(shipFactory.orderShip(name, email));
     }
 
 }
