d31947263a3b822c2a8bb21031a6dd495d1aa64a
parkhuiwo0 <huiwo0@naver.com>
2021-12-05 00:42:33 +0900

    [Bridge] - 브릿지 패턴 적용 전

---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/bridge/_01_before/KDA카이사.java
@@ -0,0 +1,4 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.bridge._01_before;
+
+public class KDA카이사 {
+}
---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/bridge/_01_before/PoolParty카이사.java
@@ -0,0 +1,4 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.bridge._01_before;
+
+public class PoolParty카이사 {
+}
