bdc9138fc4256766174dc809a9f01108612c54b0
parkhuiwo0 <huiwo0@naver.com>
2021-12-07 01:18:05 +0900

    [Facade] - 퍼사드 패턴 적용 후

---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/facade/_02_after/EmailSettings.java
@@ -0,0 +1,14 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.facade._02_after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
