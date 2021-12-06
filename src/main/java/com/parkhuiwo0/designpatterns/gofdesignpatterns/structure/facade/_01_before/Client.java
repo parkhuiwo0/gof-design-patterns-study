b86c5036e38fa573ea1f7c54df3a145fb2e4f71b
parkhuiwo0 <huiwo0@naver.com>
2021-12-07 00:53:25 +0900

    [Facade] - 퍼사드 패턴 적용 전

---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/facade/_01_before/Client.java
@@ -0,0 +1,38 @@
+//package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.facade._01_before;
+//
+//import javax.mail.Message;
+//import javax.mail.MessagingException;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
+//import java.util.Properties;
+//
+//public class Client {
+//
+//    public static void main(String[] args) {
+//        String to = "keesun@whiteship.me";
+//        String from = "whiteship@whiteship.me";
+//        String host = "127.0.0.1";
+//
+/**
+ * 클라이언트가 프로퍼티부터 마임메세지, 익셉션 등 의존성이 굉장히 많이 생긴다.
+ */
+//        Properties properties = System.getProperties();
+//        properties.setProperty("mail.smtp.host", host);
+//
+//        Session session = Session.getDefaultInstance(properties);
+//
+//        try {
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress(from));
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//            message.setSubject("Test Mail from Java Program");
+//            message.setText("message");
+//
+//            Transport.send(message);
+//        } catch (MessagingException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
