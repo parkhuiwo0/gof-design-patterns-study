343a6180848f79185f9ac740d358f60e615cc3ad
parkhuiwo0 <huiwo0@naver.com>
2021-12-06 23:29:01 +0900

    [Decorator] - 데코레이터 패턴 추가 전

---
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/decorator/_01_before/CommentService.java
@@ -0,0 +1,7 @@
+package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.decorator._01_before;
+
+public class CommentService {
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
