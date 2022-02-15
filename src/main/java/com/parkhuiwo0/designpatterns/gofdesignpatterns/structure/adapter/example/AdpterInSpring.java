af32f1dc7aef944f758a74a2fcd9332754195d1b
parkhuiwo0 <huiwo0@naver.com>
2022-02-16 01:06:42 +0900

    Spring Example을 수정하라

--- src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/adapter/example/AdpterInSpring.java
+++ src/main/java/com/parkhuiwo0/designpatterns/gofdesignpatterns/structure/adapter/example/AdpterInSpring.java
@@ -1,12 +1,13 @@
 package com.parkhuiwo0.designpatterns.gofdesignpatterns.structure.adapter.example;
 
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 public class AdpterInSpring {
     public static void main(String[] args) {
         DispatcherServlet dispatcherServlet = new DispatcherServlet();
-        RequestMappingHandlerAdapter requestMappingHandlerAdapter = new RequestMappingHandlerAdapter();
+        HandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
 
     }
 }
